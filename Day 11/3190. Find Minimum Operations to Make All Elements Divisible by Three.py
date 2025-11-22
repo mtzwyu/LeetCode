372f76a85a58576a72c9a3a8e279ca6e4368ff4a
mtzwyu <mtruong2509@gmail.com>
2025-11-22 23:21:48 +0700

    Day 12

--- Day 11/3190. Find Minimum Operations to Make All Elements Divisible by Three.py
+++ Day 11/3190. Find Minimum Operations to Make All Elements Divisible by Three.py
@@ -1,9 +1,5 @@
 class Solution:
     def minimumOperations(nums: list[int]) -> int:
-        res = 0
-        for i in nums:
-            if (i+1) % 3 == 0 or (i - 1) % 3 == 0:
-                res +=1 
-        return res
+        return sum(1 for i in nums if i % 3 == 2 or i % 3 == 1)
 
 print(Solution.minimumOperations([1,2,3,4]))
