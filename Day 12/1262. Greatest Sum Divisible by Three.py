96a58aa55398275a3e425a1efe4d2f916644765f
mtzwyu <mtruong2509@gmail.com>
2025-11-23 15:35:24 +0700

    Day 12

---
+++ Day 12/1262. Greatest Sum Divisible by Three.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maxSumDivThree(nums: list[int]) -> int:
+        s = sum(nums)
+        
+        if s % 3 == 0:
+            return s
+        
+        r11 = 10000
+        r12 = 10000
+        r21 = 10000
+        r22 = 10000
+        
+        for num in nums:
+            if num % 3 == 1 and num < r12:
+                if num < r11:
+                    r12 = r11
+                    r11 = num
+                else:
+                    r12 = num
+            if num % 3 == 2 and num < r22:
+                if num < r21:
+                    r22 = r21
+                    r21 = num
+                else: 
+                    r22 = num
+        if s % 3 == 1:
+            return s - min(r11, r21+r22)
+        if s % 3 == 2:
+            return s - min(r21, r11+r12) 
+        
+        
+print(Solution.maxSumDivThree([2,6,2,2,7])) # output: 15
+        
