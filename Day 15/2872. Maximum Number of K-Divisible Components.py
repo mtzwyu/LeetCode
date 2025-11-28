c93e705cb312d228bed4cf8c765a35aad9dbf7a3
mtzwyu <mtruong2509@gmail.com>
2025-11-28 23:31:20 +0700

    toi uu Day 15 de hieu

--- Day 15/2872. Maximum Number of K-Divisible Components.py
+++ Day 15/2872. Maximum Number of K-Divisible Components.py
@@ -1,24 +1,24 @@
 class Solution:
     def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
-        
-        def dfs(node, parent):
-            cursum = values[node]
-            for nei in adj_list[node]:
-                if nei == parent: continue
-                cursum += dfs(nei, node)
-            if cursum % k==0:
-                self.ans += 1
-                return 0
-            return cursum
-
-        adj_list = [[] for _ in range(n)]
-        for u,v in edges:
-            adj_list[u].append(v)
-            adj_list[v].append(u)
-
-        print(adj_list)
-        self.ans = 0
+        graph = [[] for _ in range(n)]
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        self.components_count = 0
+        def dfs(current_node, parent_node):
+            current_sum = values[current_node]
+            for neighbor in graph[current_node]:
+                if neighbor != parent_node:
+                    child_remainder = dfs(neighbor, current_node)
+                    
+                    current_sum += child_remainder
+            if current_sum % k == 0:
+                self.components_count += 1
+                return 0 
+            else:
+                return current_sum
         dfs(0, -1)
-        return self.ans
+        
+        return self.components_count
     
 print(Solution().maxKDivisibleComponents(n = 7, edges = [[0,1],[0,2],[1,3],[1,4],[2,5],[2,6]], values = [3,0,6,1,5,2,1], k = 3))
