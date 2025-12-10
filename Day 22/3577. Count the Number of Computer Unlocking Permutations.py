27d54eb660d5eca01eead9490c7f6f6b3ca2837b
mtzwyu <mtruong2509@gmail.com>
2025-12-10 17:54:14 +0700

    toii uu day 22

--- Day 22/3577. Count the Number of Computer Unlocking Permutations.py
+++ Day 22/3577. Count the Number of Computer Unlocking Permutations.py
@@ -1,14 +1,12 @@
+import math
 class Solution:
     def countPermutations(self, complexity: List[int]) -> int:
         n = len(complexity)
         for i in range(1, n):
             if complexity[i] <= complexity[0]:
                 return 0
-
-        ans, mod = 1, 10**9 + 7
-        for i in range(2, n):
-            ans = ans * i % mod
-        return ans
+        
+        return math.factorial(n-1) % (10**9 + 7)
     
     
     
